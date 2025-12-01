32c1249928ee0cfefed92994c9cfe8c70bd9902c
Boxfriend <cawills394@gmail.com>
2025-12-01 14:12:04 -0500

    brute forced part two

--- AdventOfCode2025/SecretEntrance.cs
+++ AdventOfCode2025/SecretEntrance.cs
@@ -8,15 +8,25 @@ public class SecretEntrance : IAdventSolution
         
         var current = 50;
         var password = 0;
+        var password2 = 0;
         foreach (var line in lines)
         {
             var direction = line[0] == 'L' ? -1 : 1;
-            var distance =  int.Parse(line.Substring(1)) * direction;
-            current += distance;
+            var distance =  int.Parse(line.Substring(1));
+            var change = distance * direction;
+            for (var temp = current; distance > 0; distance--)
+            {
+                temp += direction;
+                temp %= 100;
+                if(temp == 0) password2++;
+            }
+
+            
+            current += change;
             current %= 100;
             if(current == 0) password++;
         }
         
-        return new AdventSolution(password.ToString(), null);
+        return new AdventSolution(password.ToString(), password2.ToString());
     }
 }

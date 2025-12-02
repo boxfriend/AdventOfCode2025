8dbf5946eed68ad3bcb2e9218a3fba705874a363
Boxfriend <cawills394@gmail.com>
2025-12-02 10:31:01 -0500

    day two part one

---
+++ AdventOfCode2025/GiftShop.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2025;
+
+public class GiftShop : IAdventSolution
+{
+    public AdventSolution Solve(string input)
+    {
+        var ranges = input.Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        ulong totalInvalid = 0;
+        foreach (var rangeString in ranges)
+        {
+            var range = GetRange(rangeString);
+            for (var i = range.Min; i <= range.Max; i++)
+            {
+                if(IsInvalid(i))
+                    totalInvalid += (ulong)i;
+                    
+            }
+        }
+        
+        return new AdventSolution(totalInvalid.ToString(), null);
+    }
+
+    private bool IsInvalid(ulong id)
+    {
+        var s = id.ToString();
+        var half = s.Length / 2;
+        var first = s[..half];
+        var second = s[half..];
+        return first == second;
+    }
+
+    private Range GetRange(string input)
+    {
+        var numbers = input.Split('-', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        return new Range(ulong.Parse(numbers[0]), ulong.Parse(numbers[^1]));
+    }
+}
+
+public record struct Range(ulong Min, ulong Max);
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -4,3 +4,6 @@ Console.WriteLine("Advent of Code 2025");
 
 var dayOneInput = await File.ReadAllTextAsync("./Input/DayOne.txt");
 AdventSolver<SecretEntrance>.Solve(dayOneInput, 1, "Secret Entrance");
+
+var dayTwoInput = await File.ReadAllTextAsync("./Input/DayTwo.txt");
+AdventSolver<GiftShop>.Solve(dayTwoInput, 2, "Gift Shop");

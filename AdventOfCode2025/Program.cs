633a9d92481be7d4733dab81fe072e7af2719f26
Boxfriend <cawills394@gmail.com>
2025-12-03 12:50:31 -0500

    prepping for day two

--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -7,3 +7,6 @@ AdventSolver<SecretEntrance>.Solve(dayOneInput, 1, "Secret Entrance");
 
 var dayTwoInput = await File.ReadAllTextAsync("./Input/DayTwo.txt");
 AdventSolver<GiftShop>.Solve(dayTwoInput, 2, "Gift Shop");
+
+var dayThreeInput = await File.ReadAllTextAsync("./Input/Test.txt");
+AdventSolver<Lobby>.Solve(dayThreeInput, 3, "Lobby");

1487a1cd2732f913dfd0b0e9a3f50fb0921cc541
Boxfriend <cawills394@gmail.com>
2025-12-04 00:31:43 -0500

    utility Coordinate.cs

---
+++ AdventOfCode2025/Utils/Coordinate.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+namespace AdventOfCode2025.Utils;
+
+public record struct Coordinate(int X, int Y) : IAdditionOperators<Coordinate,Coordinate,Coordinate>, ISubtractionOperators<Coordinate,Coordinate,Coordinate>
+{
+    public static Coordinate operator +(Coordinate left, Coordinate right)
+    {
+        return new Coordinate(left.X + right.X, left.Y + right.Y);
+    }
+
+    public static Coordinate operator -(Coordinate left, Coordinate right)
+    {
+        return new Coordinate(left.X - right.X, left.Y - right.Y);
+    }
+}

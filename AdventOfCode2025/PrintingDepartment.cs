31780b1dcaa0f4cfb93ed4bcfb45ef088feee045
Boxfriend <cawills394@gmail.com>
2025-12-04 00:14:16 -0500

    prepping Day 4

---
+++ AdventOfCode2025/PrintingDepartment.cs
@@ -0,0 +1,9 @@
+﻿namespace AdventOfCode2025;
+
+public class PrintingDepartment : IAdventSolution
+{
+    public AdventSolution Solve(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -10,3 +10,6 @@ AdventSolver<GiftShop>.Solve(dayTwoInput, 2, "Gift Shop");
 
 var dayThreeInput = await File.ReadAllTextAsync("./Input/DayThree.txt");
 AdventSolver<Lobby>.Solve(dayThreeInput, 3, "Lobby");
+
+var dayFourInput = await File.ReadAllTextAsync("./Input/Test.txt");
+AdventSolver<PrintingDepartment>.Solve(dayFourInput, 4, "Printing Department");

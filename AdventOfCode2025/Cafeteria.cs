c4ad0b41e83765fe92acd1e3c34dade4733cf55f
Boxfriend <cawills394@gmail.com>
2025-12-05 12:01:03 -0500

    part one complete

--- AdventOfCode2025/Cafeteria.cs
+++ AdventOfCode2025/Cafeteria.cs
@@ -1,9 +1,35 @@
-﻿namespace AdventOfCode2025;
+﻿using AdventOfCode2025.Utils;
+using Range = AdventOfCode2025.Utils.Range;
+
+namespace AdventOfCode2025;
 
 public class Cafeteria : IAdventSolution
 {
     public AdventSolution Solve(string input)
     {
-        throw new NotImplementedException();
+        var lines = input.SplitLines();
+
+        var ranges = new HashSet<Range>();
+        var startIndex = 0;
+        while (lines[startIndex].Contains("-"))
+        {
+            var line = lines[startIndex++];
+            var parts = line.Split('-');
+            ranges.Add(new(ulong.Parse(parts[0]), ulong.Parse(parts[1])));
+        }
+        
+        var fresh = 0;
+        for(; startIndex < lines.Length; startIndex++)
+        {
+            var line = lines[startIndex++];
+            if (line.Contains('-'))
+                continue;
+
+            var num = ulong.Parse(line);
+            if(ranges.Any(x => x.Min <= num && num <= x.Max)) fresh++;
+        }
+
+        
+        return new AdventSolution(fresh.ToString(), null);
     }
 }

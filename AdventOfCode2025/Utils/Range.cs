4a894868983902f7093787d3cb9276891c75c7eb
Boxfriend <cawills394@gmail.com>
2025-12-05 12:32:58 -0500

    utility method and property

--- AdventOfCode2025/Utils/Range.cs
+++ AdventOfCode2025/Utils/Range.cs
@@ -1,3 +1,8 @@
 ﻿namespace AdventOfCode2025.Utils;
 
-public record struct Range(ulong Min, ulong Max);
+public record struct Range(ulong Min, ulong Max)
+{
+    public ulong InclusiveCount => Max - Min + 1;
+    
+    public bool IsInRange(ulong value) => value >= Min && value <= Max;
+}
